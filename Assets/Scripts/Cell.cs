33e0483767ec61e8114d557bb4c2fda86f983c73
Jack Rayder <jrayder@mail.ru>
2021-05-26 11:14:15 +0300

    New Version

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class Cell : MonoBehaviour
+    {
+        public int CellNumber;
+        public GameManager _gameManager;
+        public GameObject ResetButton;
+        public void Check()
+        {
+            if (_gameManager.isRoundOne && CellNumber == _gameManager.WinNumber)
+            {
+                _gameManager.isRoundOne = false;
+                _gameManager.isRoundTwo = true;
+                _gameManager.ClearField(_gameManager.Set);
+               _gameManager.Round2();
+            }
+            if (_gameManager.isRoundTwo && CellNumber == _gameManager.WinNumber)
+            {
+                _gameManager.isRoundTwo = false;
+                _gameManager.isRoundThree = true;
+                _gameManager.ClearField(_gameManager.Set);
+                _gameManager.Round3();
+
+            }
+            if (_gameManager.isRoundThree && CellNumber == _gameManager.WinNumber)
+            {
+                ResetButton.SetActive(true);
+                _gameManager.isRoundThree = false;
+                _gameManager.ClearField(_gameManager.Set);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Test
+{
+    public class GameManager : MonoBehaviour
+    {
+        public GameObject CellPref;
+        public Transform Parent;
+        public List<Sprite> CellLetters;
+        public List<Sprite> CellNumbers;
+        public List<Sprite> NumbersSpare;
+        public List<Sprite> LettersSpare;
+        private int CellNumber = 0;
+        public GameManager _gameManager;
+        public GameObject _reset;
+        public int WinNumber;
+        public int Set;
+        public bool isRoundOne = true;
+        public bool isRoundTwo = false;
+        public bool isRoundThree = false;
+
+
+        private void Start()
+        {
+            NumbersSpare = new List<Sprite>(CellNumbers);
+            LettersSpare = new List<Sprite>(CellLetters);
+            Round1();
+        }
+
+        public void Round1()
+        {
+            Set = Random.Range(1, 3);
+
+            WinNumber = Random.Range(1, 4);
+            int i = 0;
+            while (i++ < 3)
+                Create(Set);
+        }
+        public void Round2()
+        {
+            Set = Random.Range(1, 3);
+            WinNumber = Random.Range(1, 7);
+            int i = 0;
+            while (i++ < 6)
+                Create(Set);
+        }
+        public void Round3()
+        {
+            Set = Random.Range(1, 3);
+            WinNumber = Random.Range(1, 10);
+            int i = 0;
+            while (i++ < 9)
+                Create(Set);
+        }
+
+        public void ClearField(int set)
+        {
+            CellNumber = 0;
+            GameObject[] clear = GameObject.FindGameObjectsWithTag("Cell");
+            for (int i = 0; i < clear.Length; i++)
+            {
+                if (clear == null) return;
+                Destroy(clear[i]);
+            }
+            CellNumbers = new List<Sprite>(NumbersSpare);
+            CellLetters = new List<Sprite>(LettersSpare);
+
+        }
+        private void Create(int set)
+        {
+            CellNumber++;
+            GameObject NewCell = Instantiate(CellPref, Parent, false);
+            NewCell.GetComponent<Cell>().CellNumber = CellNumber;
+            NewCell.GetComponent<Cell>()._gameManager = _gameManager;
+            NewCell.GetComponent<Cell>().ResetButton = _reset;
+
+            if (set == 1) 
+            {
+                int randomImage = Random.Range(0, CellLetters.Count);
+                NewCell.GetComponent<Image>().sprite = CellLetters[randomImage];
+                CellLetters.RemoveAt(randomImage);
+            }
+
+            if (set == 2)
+            {
+                int randomImage = Random.Range(0, CellNumbers.Count);
+                NewCell.GetComponent<Image>().sprite = CellNumbers[randomImage];
+                CellNumbers.RemoveAt(randomImage);
+            }
+        }
+        public void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
